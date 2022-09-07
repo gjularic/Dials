02ea7b2000f1260c010fb88f8e1018d47c528ea0
Aronxtr <gvozdan.jularic@gmail.com>
2022-09-07 12:11:36 +0000

    Added quantity form and adjust_bag view

--- bag/views.py
+++ bag/views.py
@@ -21,3 +21,24 @@ def add_to_bag(request, item_id):
     request.session['bag'] = bag
     return redirect(redirect_url)
 
+
+def adjust_bag(request, item_id):
+    """Adjust the quantity of the specified product to the specified amount"""
+
+    product = get_object_or_404(Product, pk=item_id)
+    quantity = int(request.POST.get('quantity') or 1)
+    bag = request.session.get('bag', {})
+
+    if quantity > 0:
+        bag[item_id] = quantity
+        messages.success(request, f'Updated {product.name} in bag')
+    elif quantity > 0:
+        del bag[item_id]
+        if not bag[item_id]:
+            bag.pop(item_id)
+    else:
+        bag.pop(item_id)
+        messages.success(request, f'Removed {product.name} from bag')
+
+    request.session['bag'] = bag
+    return redirect(reverse('view_bag'))
