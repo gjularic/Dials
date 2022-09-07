4d1e1dc4c31a5237446c6732711ca83ac127f37a
Aronxtr <gvozdan.jularic@gmail.com>
2022-09-07 23:01:58 +0000

    Completed webhook handler

--- checkout/views.py
+++ checkout/views.py
@@ -49,6 +49,10 @@ def checkout(request):
 
         if order_form.is_valid():
             order = order_form.save()
+            pid = request.POST.get('client_secret').split('_secret')[0]
+            order.stripe_pid = pid
+            order.original_bag = json.dumps(bag)
+            order.save()
             for item_id, item_data in bag.items():
                 try:
                     product = Product.objects.get(id=item_id)
