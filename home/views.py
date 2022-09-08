317e941233bebb73fb8b4478f40513ce68101e7b
Aronxtr <gvozdan.jularic@gmail.com>
2022-09-08 21:11:52 +0000

    Fixed bug with 404 page

--- home/views.py
+++ home/views.py
@@ -5,3 +5,8 @@ def index(request):
     """ View to return the index page """
 
     return render(request, 'home/index.html')
+
+
+def handler404(request, exception):
+    """ Error Handler 404 - Page Not Found """
+    return render(request, "errors/404.html", status=404)
